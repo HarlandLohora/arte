62eb30dd7a9d74d6380a32b7c785e316b71261cc
HarlandLohora <harland@lohora.com>
2022-09-16 11:20:41 -0300

    cambios

---
+++ models/Comentarios.model.js
@@ -0,0 +1,23 @@
+const { Schema, model } = require("mongoose");
+
+const commentsSchema = new Schema(
+  {
+    content: {
+      type: String,
+    },
+    date: {
+      type: Date,
+    },
+    author: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Comments = model("Comments", commentsSchema);
+
+module.exports = Comments;
